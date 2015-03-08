836fa6d0935bd47721ca7faf37e20b2afaa7e7ea
Herbert G. Fischer <herbert.fischer@gmail.com>
2015-03-08 12:34:02 -0300

    Work with Google Authenticator

--- otp/main.go
+++ otp/main.go
@@ -4,32 +4,54 @@ import (
 	"encoding/base32"
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/hgfischer/go-otp"
 )
 
 var (
-	secret  = flag.String("secret", "secret", "Secret")
-	length  = flag.Uint("length", otp.DefaultLength, "OTP length")
-	period  = flag.Uint("period", otp.DefaultPeriod, "Period in seconds")
-	counter = flag.Uint64("counter", 0, "Counter")
+	secret   = flag.String("secret", "AAAABBBBCCCCDDDD", "Secret key")
+	isBase32 = flag.Bool("base32", true, "If true, the secret is interpreted as a Base32 string")
+	length   = flag.Uint("length", otp.DefaultLength, "OTP length")
+	period   = flag.Uint("period", otp.DefaultPeriod, "Period in seconds")
+	counter  = flag.Uint64("counter", 0, "Counter")
 )
 
 func main() {
 	flag.Parse()
-	key, _ := base32.StdEncoding.DecodeString(*secret)
+
+	key := *secret
+	if !*isBase32 {
+		key = base32.StdEncoding.EncodeToString([]byte(*secret))
+	}
+
+	key = strings.ToUpper(key)
+	if !isGoogleAuthenticatorCompatible(key) {
+		fmt.Println("WARN: Google Authenticator requires 16 chars base32 secret, without padding")
+	}
+
+	fmt.Println("Secret Base32 Encoded Key: ", key)
 
 	totp := &otp.TOTP{
-		Secret: string(key),
-		Length: uint8(*length),
-		Period: uint8(*period),
+		Secret:         key,
+		Length:         uint8(*length),
+		Period:         uint8(*period),
+		IsBase32Secret: true,
 	}
 	fmt.Println("TOTP:", totp.Get())
 
 	hotp := &otp.HOTP{
-		Secret:  string(key),
-		Length:  uint8(*length),
-		Counter: *counter,
+		Secret:         key,
+		Length:         uint8(*length),
+		Counter:        *counter,
+		IsBase32Secret: true,
 	}
+
 	fmt.Println("HOTP:", hotp.Get())
+}
+
+func isGoogleAuthenticatorCompatible(base32Secret string) bool {
+	cleaned := strings.Replace(base32Secret, "=", "", -1)
+	cleaned = strings.Replace(cleaned, " ", "", -1)
+	return len(cleaned) == 16
 }
